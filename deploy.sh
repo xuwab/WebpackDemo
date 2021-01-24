537d1ab751119fdd37ad85308e6639a68291a505
wab <xuwab@outlook.com>
2021-01-24 16:02:56 +0800

    update

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-npm run build &&
-git checkout GitHub_Pages &&
-rm -rf src &&
-rm -rf *.json *.html *.sh *.png &&
-mv dist/* ./ &&
-rm -rf dist;
-git add . &&
-git commit -m 'update' &&
-git push
--- src/lazy.js
+++
@@ -1,3 +0,0 @@
-export default function lazy(){
-    console.log('This is a lazy loader')
-}
